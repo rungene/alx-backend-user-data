7a0738d36ac8ab8d857c57679e1ef436d0558a5e
rungene <rungenesamuel@gmail.com>
2023-09-01 02:14:34 +0000

    Task: 6. Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -17,3 +17,20 @@ def hash_password(password: str) -> bytes:
         salted, hashed password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Checks if password is valid
+
+    Args:
+        hashed_password:(bytes) this is the hashed password
+        password:(str) password to confirm entered by user
+
+    Return:
+        bool
+    """
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    else:
+        return False
