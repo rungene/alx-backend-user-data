1d65eae90d0e0b439004231988a8abce3fca2570
rungene <rungenesamuel@gmail.com>
2023-09-05 12:34:43 +0000

    Corrections

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,10 +11,19 @@ class Auth:
     Manage the API authentication
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """if path is not in excluded paths returns true,
+        hence authentication is required
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """
+        Return None
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Return None
+        """
         return None
