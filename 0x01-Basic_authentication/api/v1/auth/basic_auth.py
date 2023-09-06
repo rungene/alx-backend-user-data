00b48c11d761163c4b0949e55ac64e8c49a64ac8
rungene <rungenesamuel@gmail.com>
2023-09-06 08:53:44 +0000

    7. Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,18 @@ class BasicAuth(Auth):
     """
     Basic Auth
     """
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) \
+            -> str:
+        """
+        Returns the Base64 part of the Authorization header for
+        a Basic Authentication"""
+        if not authorization_header or not \
+                isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        auth_type, encoded_credentials = authorization_header.split(' ')
+
+        return encoded_credentials
