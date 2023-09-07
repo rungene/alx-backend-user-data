ce865944279ba3f06ca9c2c836ccad2ff042ed9f
rungene <rungenesamuel@gmail.com>
2023-09-07 11:37:27 +0000

    Task 5. Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -35,11 +35,13 @@ def filter_request() -> None:
     excluded_path = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
     if auth:
         if auth.require_auth(request.path, excluded_path):
-            if not auth.authorization_header(request):
+            if not auth.authorization_header(request) and not \
+                    auth.session_cookie(request):
                 abort(401)
 
             if not auth.current_user(request):
