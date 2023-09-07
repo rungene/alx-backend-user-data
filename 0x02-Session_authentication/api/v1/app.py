5eabb35bd10d18f0dfc151495194808bdffe2bce
rungene <rungenesamuel@gmail.com>
2023-09-07 09:18:20 +0000

    Corrections

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -19,7 +19,7 @@ auth_type = os.environ.get('AUTH_TYPE')
 if auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif auth_type = 'session_auth':
+elif auth_type == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 else:
