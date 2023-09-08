7369dc685073ab7c373fb4c98ab01e2a123449b9
rungene <rungenesamuel@gmail.com>
2023-09-08 08:54:26 +0000

    Adding a new routes. DELETE

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -34,3 +34,13 @@ def session_login():
             response.set_cookie(cookie_name, session_id)
             return response
     return jsonify({"error": "wrong password"}), 401
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def session_logout():
+    """Implements deleting the session id"""
+    from api.v1.app import auth
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200
