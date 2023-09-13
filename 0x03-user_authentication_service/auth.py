3d62e0d8cd276b7cef4b0c0b9d04c54a53518017
rungene <rungenesamuel@gmail.com>
2023-09-13 12:08:25 +0000

    Adding annotations

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -28,7 +28,7 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email, password) -> User:
+    def register_user(self, email: str, password: str) -> User:
         """Registers user, save to db
         Args:
             email(str): user email
