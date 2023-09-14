cf7cadcf7703a1aaafd23365da0c365799e93f14
rungene <rungenesamuel@gmail.com>
2023-09-14 18:49:03 +0000

    Corrections

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -75,7 +75,7 @@ def get_reset_password_token():
     """respond to the POST /reset_password route"""
     email = request.form.get('email')
     try:
-        reset_token = get_reset_password_token(email)
+        reset_token = AUTH.get_reset_password_token(email)
         return jsonify({"email": email, "reset_token": reset_token}), 200
     except ValueError:
         abort(403)
