3872a35a127c5bf98822821d787f99682596be75
rungene <rungenesamuel@gmail.com>
2023-09-14 11:51:54 +0000

    Corrections

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -48,7 +48,7 @@ def login():
 @app.route('/sessions' methods=['DELETE', strict_slashes=False])
 def logout():
     """implements logout"""
-    session_id = request.cookies.get('session_id"')
+    session_id = request.cookies.get('session_id')
     if session_id:
         user = AUTH.get_user_from_session_id(session_id)
         if not user:
