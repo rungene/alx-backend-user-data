72a1b2d565c6d0386187157dae7465b1bb69c300
rungene <rungenesamuel@gmail.com>
2023-09-15 02:59:37 +0000

    test

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -2,13 +2,36 @@
 """
 Main file
 """
-from auth import Auth
+import unittest
+import requests
+from app import app
 
-email = 'bob@bob.com'
-password = 'MyPwdOfBob'
-auth = Auth()
+BASE_URL = 'http://localhost:5000'
 
-auth.register_user(email, password)
 
-print(auth.create_session(email))
-print(auth.create_session("unknown@email.com"))
+class TestEndPoints(unittest.TestCase):
+    """End to end integrations Test"""
+    def test_register_user(self):
+        response = request.post(f'{BASE_URL}/users',
+                                data={'email': 'rungene@me.com',
+                                      'password': 'pass'})
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('user created', response.text)
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
